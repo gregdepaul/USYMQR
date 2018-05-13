4f37df80478169b15e5d9711105e45820c616e9d
gregdepaul <gdepaul@stanford.edu>
2018-05-13 11:29:44 -0700

    Correct Code (for once)

--- tri_test.jl
+++ tri_test.jl
@@ -1,64 +1,6 @@
-function tridiagonalization(A)
+include("tridiagonalization.jl")
 
-    (m,m) = size(A);
-    b = randn(1 ,m);
-    c = randn(1, m);
-
-    beta = [];
-    gamma = [];
-    alpha = [];
-
-    push!(beta, norm(b));
-    push!(gamma, norm(c));
-
-    P = zeros(m,m);
-    Q = zeros(m,m);
-
-    P[:, 1] = b / beta[1]
-    Q[:, 1] = c / gamma[1]
-
-    i = 1;
-    maxiters = m;
-    while i < maxiters
-
-        u = A*Q[:, i];
-        v = A'*P[:, i];
-
-        if i > 1
-            u = u - gamma[i]*P[:, i - 1];
-            v = v - beta[i]*Q[:, i - 1];
-        end
-
-        push!(alpha, P[:, i]'*u);
-
-        u = u - alpha[i]*P[: ,i];
-        v = v - alpha[i]'*Q[:, i];
-
-        if (norm(u) == 0 || norm(v) == 0)
-            break;
-        else
-            push!(beta, norm(u));
-            push!(gamma, norm(v));
-
-            P[:, i] = u/beta[i+1];
-            Q[:, i] = v/gamma[i+1];
-        end
-
-        i = i + 1;
-    end
-
-    betaFT = convert(Array{Float64,1}, beta);
-    alphaFT = convert(Array{Float64,1}, alpha);
-    gammaFT = convert(Array{Float64,1}, gamma);
-
-    T = Tridiagonal(betaFT[2:size(alphaFT,1)], alphaFT, gammaFT[2:size(alphaFT,1)]);
-
-    P = P[:, 1:length(alphaFT)];
-    Q = Q[:, 1:length(alphaFT)];
-
-    return P, T, Q;
-
-    end
+# A small code to test tridiagonalization file
 
 A = rand(10, 10)
 (P, T, Q) = tridiagonalization(A)
--- tridiagonalization.jl
+++ tridiagonalization.jl
@@ -1,56 +1,61 @@
-A = rand(10, 10);
+function tridiagonalization(A)
 
-(m,n) = size(A);
-b = randn(1 ,n);
-c = randn(1, m);
+    (m,m) = size(A);
+    b = randn(1 ,m);
+    c = randn(1, m);
 
-beta = [];
-gamma = [];
-alpha = [];
+    beta = [];
+    gamma = [];
+    alpha = [];
 
-push!(beta, norm(b));
-push!(gamma, norm(c));
+    push!(beta, norm(b));
+    push!(gamma, norm(c));
 
-P = zeros(m,n);
-Q = zeros(m,n);
+    P = zeros(m,m);
+    Q = zeros(m,m);
 
-P[:, 1] = b / beta[1]
-Q[:, 1] = c / gamma[1]
+    P[:, 1] = b / beta[1]
+    Q[:, 1] = c / gamma[1]
 
-i = 1;
-u = A*Q[:, i];
-v = ctranspose(A)*P[:, i];
+    i = 1;
+    maxiters = m;
+    while i < maxiters
 
-maxiters = m;
-while i < maxiters
-    if i > 1
-        u = u - gamma[i]*P[:, i - 1];
-        v = v - beta[i]*Q[:, i - 1];
+        u = A*Q[:, i];
+        v = A'*P[:, i];
+
+        if i > 1
+            u = u - gamma[i]*P[:, i - 1];
+            v = v - beta[i]*Q[:, i - 1];
+        end
+
+        push!(alpha, P[:, i]'*u);
+
+        u = u - alpha[i]*P[: ,i];
+        v = v - alpha[i]'*Q[:, i];
+
+        if (norm(u) == 0 || norm(v) == 0)
+            break;
+        else
+            push!(beta, norm(u));
+            push!(gamma, norm(v));
+
+            P[:, i+1] = u/beta[i+1];
+            Q[:, i+1] = v/gamma[i+1];
+        end
+
+        i = i + 1;
     end
 
-    push!(alpha, ctranspose(P[:, i])*u);
+    betaFT = convert(Array{Float64,1}, beta);
+    alphaFT = convert(Array{Float64,1}, alpha);
+    gammaFT = convert(Array{Float64,1}, gamma);
 
-    #print(alpha)
-    u = u - alpha[i]*P[: ,i];
-    v = v - ctranspose(alpha[i])*Q[:, i];
+    T = Tridiagonal(betaFT[2:size(alphaFT,1)], alphaFT, gammaFT[2:size(alphaFT,1)]);
 
-    if (norm(u) == 0 || norm(v) == 0)
-        break;
-    else
-        push!(beta, norm(u));
-        push!(gamma, norm(v));
+    P = P[:, 1:length(alphaFT)];
+    Q = Q[:, 1:length(alphaFT)];
 
-        P[:, i+1] = u/beta[i+1];
-        Q[:, i+1] = v/gamma[i+1];
+    return P, T, Q;
+
     end
-
-    i = i + 1;
-end
-
-
-
-betaFT = convert(Array{Float64,1}, beta);
-alphaFT = convert(Array{Float64,1}, alpha);
-gammaFT = convert(Array{Float64,1}, gamma);
-
-T = Tridiagonal(betaFT[2:size(alphaFT,1)], alphaFT, gammaFT[2:size(alphaFT,1)]);
