8c3fbb20b4ce036f9e47b25eebca8bb9db117b6a
stephanie sanchez <ssanche2@stanford.edu>
2018-05-23 14:16:09 -0700

    working on lsqr

--- usymqr.jl
+++ usymqr.jl
@@ -3,7 +3,7 @@
 #    return 0;
 
 #end
-
+using IterativeSolvers
 include("tridiagonalization.jl")
 
 # A small code to test tridiagonalization file
@@ -13,6 +13,19 @@ A = rand(m, m)
 last_beta = zeros(1,size(T,2))
 last_beta[end] = beta;
 #augment T to get S
-S = [T ; last_beta];
+S = Array([T ; last_beta]);
+
+QR = qrfact(S);
+Q = QR[:Q];
+R = QR[:R];
+#x = minres(S,last_beta);
 
 #now do least squares with QR fact on S
+M = Q*inv(R);
+b = rand(1, size(A,1));
+x0 = rand(1, size(A,1));
+b1 = norm(b'-A*x0');
+beta1 = zeros(1,size(S,1))';
+beta1[end] = r0;
+type()
+x = lsqr(S,beta1);
